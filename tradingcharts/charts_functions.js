98d6c93070ee2e2100805103e692195e86c56b7b
MalikovMuhammad <muhammad.malikov@gmail.com>
2022-03-04 17:39:52 +0200

    sizeChangableCharts

--- tradingcharts/charts_functions.js
+++ tradingcharts/charts_functions.js
@@ -1,10 +1,9 @@
 ﻿           function valyuta(pair) {
                 if (document.getElementById(pair).style.display == "none") {
                     document.getElementById(pair).style.display = "block";
-                    document.getElementById(pair).innerHTML = '<div class="tradingview-widget-container">  <div id="tradingview_'+pair+'"></div>  <div class="tradingview-widget-copyright">	<b style="color:black;">График '+pair+' '+ timeframe +' </b>  </div>';                
+                    document.getElementById(pair).innerHTML = '<div class="tradingview-widget-container">  <div id="tradingview_'+pair+'"></div>  <div class="tradingview-widget-copyright">	<b style="color:black;">График '+pair+' '+ timeframe +' </b>  </div></div>';                
                     new TradingView.widget({
-                        "width": 450,
-                        "height": 280,
+                        "autosize": true,
                         "symbol": "FOREXCOM:"+pair,
                         "interval": timeframe,
                         "timezone": "Asia/Bahrain",
@@ -30,10 +29,9 @@
            function kripto(pair) {
                 if (document.getElementById(pair).style.display == "none") {
                     document.getElementById(pair).style.display = "block";
-                    document.getElementById(pair).innerHTML = '<div class="tradingview-widget-container">  <div id="tradingview_'+pair+'"></div>  <div class="tradingview-widget-copyright">	<b style="color:black;">График '+pair+' '+ timeframe +'  </b>  </div>';                
+                    document.getElementById(pair).innerHTML = '<div class="tradingview-widget-container">  <div id="tradingview_'+pair+'"></div>  <div class="tradingview-widget-copyright">	<b style="color:black;">График '+pair+' '+ timeframe +'  </b>  </div></div>';                
                     new TradingView.widget({
-                        "width": 450,
-                        "height": 280,
+                        "autosize": true,
                         "symbol": "BINANCE:"+pair,
                         "interval": timeframe,
                         "timezone": "Asia/Bahrain",
@@ -55,3 +53,97 @@
                     document.getElementById(pair).style.display = "none";                    
                 }                
 	       }       
+           
+    //Изменение ширины графиков
+    function setChartWidth(){
+        var width = document.getElementById('chartWidth').value;
+        
+        document.getElementById('EURUSD').style.width = width;
+        document.getElementById('GBPUSD').style.width = width;
+        document.getElementById('AUDUSD').style.width = width;
+        document.getElementById('NZDUSD').style.width = width;
+        document.getElementById('USDCAD').style.width = width;
+        document.getElementById('USDCHF').style.width = width;
+        document.getElementById('USDJPY').style.width = width;
+        
+        document.getElementById('EURGBP').style.width = width;
+        document.getElementById('EURAUD').style.width = width;
+        document.getElementById('EURNZD').style.width = width;
+        document.getElementById('EURCAD').style.width = width;
+        document.getElementById('EURCHF').style.width = width;
+        document.getElementById('EURJPY').style.width = width;
+        
+        document.getElementById('GBPAUD').style.width = width;
+        document.getElementById('GBPNZD').style.width = width;
+        document.getElementById('GBPCAD').style.width = width;
+        document.getElementById('GBPCHF').style.width = width;
+        document.getElementById('GBPJPY').style.width = width;
+        
+        document.getElementById('AUDNZD').style.width = width;
+        document.getElementById('AUDCAD').style.width = width;
+        document.getElementById('AUDCHF').style.width = width;
+        document.getElementById('AUDJPY').style.width = width;
+        
+        document.getElementById('NZDCAD').style.width = width;
+        document.getElementById('NZDCHF').style.width = width;
+        document.getElementById('NZDJPY').style.width = width;
+        
+        document.getElementById('CADCHF').style.width = width;
+        document.getElementById('CADJPY').style.width = width;
+        
+        document.getElementById('CHFJPY').style.width = width;
+        
+        document.getElementById('USDT').style.width = width;
+        document.getElementById('USDT').style.width = width;
+        document.getElementById('USDT').style.width = width;
+        document.getElementById('USDT').style.width = width;
+        document.getElementById('USDT').style.width = width;
+        document.getElementById('USDT').style.width = width;
+        document.getElementById('USDT').style.width = width;
+        document.getElementById('USDT').style.width = width;
+        document.getElementById('USDT').style.width = width;
+        document.getElementById('USDT').style.width = width;
+        
+        document.getElementById('USDT').style.width = width;
+        document.getElementById('USDT').style.width = width;
+        document.getElementById('USDT').style.width = width;
+        document.getElementById('USDT').style.width = width;
+        document.getElementById('USDT').style.width = width;
+        document.getElementById('USDT').style.width = width;
+        document.getElementById('USDT').style.width = width;
+        document.getElementById('USDT').style.width = width;
+        document.getElementById('USDT').style.width = width;
+        document.getElementById('USDT').style.width = width;
+        
+        document.getElementById('USDT').style.width = width;
+        document.getElementById('USDT').style.width = width;
+        document.getElementById('USDT').style.width = width;
+        document.getElementById('USDT').style.width = width;
+        document.getElementById('USDT').style.width = width;
+        document.getElementById('USDT').style.width = width;
+        document.getElementById('USDT').style.width = width;
+        document.getElementById('USDT').style.width = width;
+        document.getElementById('USDT').style.width = width;
+        document.getElementById('USDT').style.width = width;
+        
+        document.getElementById('USDT').style.width = width;
+        document.getElementById('USDT').style.width = width;
+        document.getElementById('USDT').style.width = width;
+        document.getElementById('USDT').style.width = width;
+        
+    }
+    
+    //Изменение высоты графиков
+    function setChartHeight(){
+        var height = document.getElementById('chartHeight').value;
+        
+        document.getElementById('EURUSD').style.height = height;
+    }
+           
+           
+           
+           
+           
+           
+           
+           
